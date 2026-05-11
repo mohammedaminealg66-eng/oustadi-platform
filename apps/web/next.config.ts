15e96dfd411cd12792ed0e8645695072ad600a69
aminalg <mohammedaminealg.66@gmail.com>
2026-05-11 12:07:26 +0100

    fix: login form submission 3333333333

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // هادي هي الطريقة الصحيحة فـ النسخة الجديدة
-  devIndicators: {
-    appIsrStatus: false,
-  },
-  // جرب هادي باش تحل مشكل الـ Blockage
-  async headers() {
-    return [
-      {
-        source: "/_next/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-        ],
-      },
-    ];
-  },
+  // @ts-ignore كنفورصيو هاد الخاصية باش السيرفر يقبل الـ IP
+  allowedDevOrigins: ['192.168.1.151', 'localhost:3000'],
 };
 
 export default nextConfig;
