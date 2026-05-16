de8fe867b384ce6e4a173211872dd832fd899f94
aminalg <mohammedaminealg.66@gmail.com>
2026-05-16 03:14:55 +0100

    bjbjde

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // @ts-ignore كنفورصيو هاد الخاصية باش السيرفر يقبل الـ IP
-  allowedDevOrigins: ['192.168.1.199', 'localhost:3000'],
+  allowedDevOrigins: ['192.168.0.106', 'localhost:3000'],
 };
 
 export default nextConfig;
